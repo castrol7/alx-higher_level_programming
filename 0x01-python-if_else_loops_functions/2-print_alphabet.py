623441c392a3ac627afc0d01b3a17f4fd21f5344
castrol7 <oluochcastrol@gmail.com>
2023-06-07 13:18:54 +0000

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
-for i in range(ord('a'), ord('z')+1):
-    print(chr(i), end='')
-
-print()
+#!/usr/bin/python3
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
