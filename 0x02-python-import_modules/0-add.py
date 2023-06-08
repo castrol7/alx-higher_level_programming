820accd1a5cdf44f230ca4abd8169761d10c68b9
castrol7 <oluochcastrol@gmail.com>
2023-06-08 17:11:36 +0000

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import add_0
+
+
+def main():
+    a = 1
+    b = 2
+
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+
+if __name__ == '__main__':
+    main()
