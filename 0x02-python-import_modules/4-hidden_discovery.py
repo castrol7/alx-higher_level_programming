28331d0b9a8dcb8994e82c2db369bbfb235ce56c
castrol7 <oluochcastrol@gmail.com>
2023-06-09 10:05:39 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 import sys
-import inspect
 
-def print_names():
-    module = sys.modules[__name__]
-    members = inspect.getmembers(module)
-    names = [name for name, _ in members if not name.startswith("__")]
+if __name__ == "__main__":
+    names = [name for name in dir() if not name.startswith("__")]
     names.sort()
     
     for name in names:
         print(name)
-
-if __name__ == "__main__":
-    print_names()
