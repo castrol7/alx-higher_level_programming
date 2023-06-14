79a5c307429367fb4af094cafdc2b8ba4ffb3996
castrol7 <oluochcastrol@gmail.com>
2023-06-14 09:13:30 -0700

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
+
+def uniq_add(my_list=None):
+    if my_list is None:
+        my_list = []
+
     new_list = set(my_list)
     total = 0
     for item in new_list:
         total += item
+
     return total
+
 
 my_list = [1, 2, 3, 1, 4, 2, 5]
 result = uniq_add(my_list)
